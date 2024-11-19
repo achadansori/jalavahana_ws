3a4fce3f5822d21ebd7403d5f0f160c09b3fb1f0
jalavahana@gmail.com <achadansori@gmail.com>
2024-11-20 03:23:42 +0700

    nyoba vision

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jetson/jalavahana_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jetson/jalavahana_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/jetson/.local/bin:/home/jetson/.nvm/versions/node/v20.16.0/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/jetson/jalavahana_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/jetson/jalavahana_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/jetson/jalavahana_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/jetson/jalavahana_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/sensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetson/jalavahana_ws/src/sensor
+# Build directory: /home/jetson/jalavahana_ws/build/sensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
